b21c742536f4f3c44a79540638e822b27b1522ae
Geordan Daugherty <geordan_daugherty@hotmail.com>
2022-10-07 23:30:20 -0500

    Functions lesson added!

---
+++ Functions/index.js
@@ -0,0 +1,26 @@
+function myFunction () {
+    for (let i = 0; i <= 100; i++) {
+        console.log(i);
+    }
+}
+// anything can go into a function loops arrays variables stay inside the function.... make sure they are only responsable for one thing if not another is needed.
+myFunction();
+
+function creatFullName(firstName, lastName) {  //peramiters (placeholders) || saves spot for called argument.
+    console.log(firstName + ' '  + lastName); //peramiters also entered here not the argument....
+} creatFullName("Geordan", "Daugherty"); // argument || passed into the placeholder......
+//peramiters are like parking spots and the arguments are the cars 
+creatFullName("Dodge", "Neon"); // just testing
+creatFullName("Honda", "CRV");// just testing
+//FUNCTION WAS COPYED BELOW!!!!
+//GIVE DISCRIPTIVE NAMES THAT MEAN SOMETHING SO ITS EASILY READABLE.
+//COPYED FUNCTION FROM ABOVE!!
+function creatFullName(firstName, lastName) { 
+    return firstName + ' '  + lastName; 
+} console.log(creatFullName("Geordan", "Daugherty"));
+// orrrrrr you can do  this
+let fullName = creatFullName("Geordan", "Daugherty");
+console.log("Welcome " + fullName);
+
+
+
