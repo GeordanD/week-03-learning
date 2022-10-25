e45d9bb7298eb44a04d38b834895ec83f94a8e02
Geordan Daugherty <geordan_daugherty@hotmail.com>
2022-10-24 20:57:16 -0500

    this is how you do it

--- Functions/index.js
+++ Functions/index.js
@@ -6,20 +6,20 @@ function myFunction () {
 // anything can go into a function loops arrays variables stay inside the function.... make sure they are only responsable for one thing if not another is needed.
 myFunction();
 
-function creatFullName(firstName, lastName) {  //peramiters (placeholders) || saves spot for called argument.
-    console.log(firstName + ' '  + lastName); //peramiters also entered here not the argument....
-} creatFullName("Geordan", "Daugherty"); // argument || passed into the placeholder......
-//peramiters are like parking spots and the arguments are the cars 
-creatFullName("Dodge", "Neon"); // just testing
-creatFullName("Honda", "CRV");// just testing
-//FUNCTION WAS COPYED BELOW!!!!
-//GIVE DISCRIPTIVE NAMES THAT MEAN SOMETHING SO ITS EASILY READABLE.
+function createFullName(firstName, lastName) {  //parameters (placeholders) || saves spot for called argument.
+    console.log(firstName + ' '  + lastName); //parameters also entered here not the argument....
+} createFullName("Geordan", "Daugherty"); // argument || passed into the placeholder......
+//parameters are like parking spots and the arguments are the cars 
+createFullName("Dodge", "Neon"); // just testing
+createFullName("Honda", "CRV");// just testing
+//FUNCTION WAS COPIED BELOW!!!!
+//GIVE DESCRIPTIVE NAMES THAT MEAN SOMETHING SO ITS EASILY READABLE.
 //COPYED FUNCTION FROM ABOVE!!
-function creatFullName(firstName, lastName) { 
+function createFullName(firstName, lastName) { 
     return firstName + ' '  + lastName; 
-} console.log(creatFullName("Geordan", "Daugherty"));
+} console.log(createFullName("Geordan", "Daugherty"));
 // orrrrrr you can do  this
-let fullName = creatFullName("Geordan", "Daugherty");
+let fullName = createFullName("Geordan", "Daugherty");
 console.log("Welcome " + fullName);
 
 let a = 4;
